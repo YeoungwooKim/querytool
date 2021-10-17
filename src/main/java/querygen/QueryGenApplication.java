dc9bfbcec0bd1afd39967bcf7dac729a04d85255
wooya <wooyastdy@gmail.com>
2021-10-17 15:37:32 +0900

    first commit

---
+++ src/main/java/querygen/QueryGenApplication.java
@@ -0,0 +1,13 @@
+package querygen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryGenApplication.class, args);
+	}
+
+}
